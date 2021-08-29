7d85eca3b6f0db360870e816906b618962566be5
Guilherme <guirafaellima@outlook.com>
2021-08-29 15:25:53 -0300

    Adicionando Number

--- Readme.md
+++ Readme.md
@@ -1,5 +1,4 @@
 # Fundamentos JS
 
-- Criei este repositório com o intuito de deixar registrado meu aprendizado em JS
+- Como mexer com dados do tipo number
 
-- Nele irei criar uma branch para cada fundamento que irei aprender
--- teste.js
+++ teste.js
@@ -1,9 +1,19 @@
-console.log('testando');
+const peso1 = 1.0;
+const peso2 = Number('2.0'); //outro tipo de declarar number
 
-/*para testar o código:
+console.log(Number.isInteger(peso2)); //verifica se o tipo do dado é inteiro
 
-- abrir terminal (powershell ou bash), e digitar node (nome do arquivo em js)
+const avaliacao1 = 9.903;
+const avaliacao2 = 6.872;
 
-- ou instalar a extensão code runner e dar um ctrl + alt + n
+const total = avaliacao1 * peso1 + avaliacao2 * peso2;
+const media = total / (peso1 + peso2);
 
-*/ 
+console.log(media.toFixed(2)); //determina quantos números vc quer após a vírgula
+console.log(media.toString(2)); //converte em binário
+console.log(typeof media);
+
+/*
+Number = função
+number = tipo de dado
+*/
