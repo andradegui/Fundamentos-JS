7a7f77589caa8b14bd904b268dfc125b52abce10
Guilherme <guirafaellima@outlook.com>
2021-08-29 13:47:11 -0300

    Adicionando Arquivo de Teste

---
+++ teste.js
@@ -0,0 +1,9 @@
+console.log('testando');
+
+/*para testar o código:
+
+- abrir terminal (powershell ou bash), e digitar node (nome do arquivo em js)
+
+- ou instalar a extensão code runner e dar um ctrl + alt + n
+
+*/ 
